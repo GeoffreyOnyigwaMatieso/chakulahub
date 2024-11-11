2aabbadc6ac5fb293e229c0a0cea93efe925d092
ndocharodgers <your_email@example.com>
2024-11-11 16:39:47 +0300

    adding meal items

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,26 @@
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
 
-const Meals = () => {
+
+const MealsPage = () => {
   return (
     <div>
-      <p>I am your First Meal</p>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created <span className = {classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself. It is easy and fun </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share"></Link>
+            Share your favorite Recipe
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]}/>
+      </main>
     </div>
   )
 }
 
-export default Meals
+export default MealsPage
