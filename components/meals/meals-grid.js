72298b511d1b4b6704822b27bc85df102c777dd0
Gmatieso <matiesogeoffrey@gmail.com>
2024-11-21 01:11:20 +0300

    using suspense & streamed responses for granular loading state management

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -1,12 +1,11 @@
 import MealItem from './meal-item';
 import classes from './meals-grid.module.css';
 
-
 export default function MealsGrid({meals}){
     //Flatten the meals array 
     const flatMeals = meals.flat();
 
-    console.log(meals)
+    // console.log(meals)
     return <ul className = {classes.meals}>
         {flatMeals.map(meal => <li key={meal.id}>
             <MealItem  {...meal}/>
